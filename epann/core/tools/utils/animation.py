9eca189d691c349ea3a90c1b4a5b1b81d59b8d30
chadwcarlson <chadwcarlson@gmail.com>
2017-05-01 20:00:34 -0400

    updated animation module

--- epann/core/tools/utils/animation.py
+++ epann/core/tools/utils/animation.py
@@ -14,19 +14,9 @@ class Animation:
 
     def __init__(self, array):
         self.fig = plt.figure()
-        self.data = array
-        self.frames = []
-
-    def update_frame(self, frame):
-        return self.data[:, :, frame]
-
-    def build_frames(self):
-        for frame in range(self.data.shape[2]):
-            im = plt.imshow(self.update_frame(frame), animated=True)
-            self.frames.append([im])
+        self.frames = [[plt.imshow(array[:, :, frame], animated=True)] for frame in range(array.shape[2])]
 
     def run(self):
-        self.build_frames()
         ani = animation.ArtistAnimation(self.fig, self.frames, interval=50, blit=True, repeat_delay=1000)
         plt.axis('off')
         plt.show()
